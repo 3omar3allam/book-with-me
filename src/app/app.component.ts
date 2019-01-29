e7eb06bac3913fb0427c28e415c57b9007279b56
Omar Allam <allamov96@gmail.com>
2019-01-29 23:03:25 +0200

    fixed header responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  mobileView = false;
+
+  ngOnInit(){
+    this.mobileView = (window.innerWidth < 576);
+    document.getElementsByTagName('section')[0].style.top = this.mobileView? '150px': '100px';
+
+    window.onresize = () => {
+      this.mobileView = (window.innerWidth < 576);
+      document.getElementsByTagName('section')[0].style.top = this.mobileView? '150px': '100px';
+    }
+    window.onscroll = () => {
+      if(this.mobileView){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementsByTagName('bwm-header')[0].style.top = '-65px';
+          document.getElementsByTagName('section')[0].style.top ='60px';
+        } else {
+          document.getElementsByTagName('bwm-header')[0].style.top = '0px';
+          document.getElementsByTagName('section')[0].style.top = '150px';
+        }
+      }
+    }
+  }
   clickHandler(){
     alert("Clicked!");
   }
