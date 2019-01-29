21b81e2978e2f34eb0193dfccb40420aad308e93
Omar Allam <allamov96@gmail.com>
2019-01-29 18:46:13 +0200

    Add header component and scss variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book-with-me';
+  
+  clickHandler(){
+    alert("Clicked!");
+  }
 }
